6b09d6195a0e7ef0bcef1fff0a9f66b027cbe427
gooddids22 <splendidotamiri@gmail.com>
2023-06-21 13:19:15 +0100

    push.c

--- push.c
+++ push.c
@@ -1,44 +1,66 @@
 #include "monty.h"
+
 /**
- * f_push - add node to the stack
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_push(stack_t **head, unsigned int counter)
-{
-	int n, j = 0, flag = 0;
+ * push_func - Addes an item to the stack
+ * @stack: stack to be appended
+ * @operand: data to be added
+ */
 
-if (bus.arg)
+void push_func(stack_t **stack, unsigned int operand)
 {
+	stack_t *temp_stack, *new;
 
-if (bus.arg[0] == '-')
-j++;
-for (; bus.arg[i] != '\0'; i++)
-{
-if (bus.arg[i] > 57 || bus.arg[i] < 48)
-flag = 1;
+	new = *stack;
+
+	/* Allocate memory for the temporary stack */
+	temp_stack = malloc(sizeof(stack_t));
+	if (!temp_stack)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	temp_stack->prev = NULL;
+	temp_stack->n = operand;
+	temp_stack->next = NULL;
+
+	if (*stack)
+	{
+		if (queueORstack.status == 200)
+		{
+			(*stack)->prev = temp_stack;
+			temp_stack->next = *stack;
+			*stack = temp_stack;
+		}
+		else if (queueORstack.status == 100)
+		{
+			while (new->next != NULL)
+				new = new->next;
+			new->next = temp_stack;
+			temp_stack->prev = new;
+		}
+	}
+	else
+		*stack = temp_stack;
+
+	freee(temp_stack);
 }
 
-if (flag == 1)
+/**
+ * pall_func - prints details of the stack
+ * @stack: Stack
+ * @operand: value
+ */
+void pall_func(stack_t **stack, unsigned int operand)
 {
-fprintf(stderr, "L%d: usage: push integer\n", counter);
-fclose(bus.file);
-free(bus.content);
-free_stack(*head);
-exit(EXIT_FAILURE);
-}}
-else
-{
-fprintf(stderr, "L%d: usage: push integer\n", counter);
-fclose(bus.file);
-free(bus.content);
-free_stack(*head);
-exit(EXIT_FAILURE);
-}
-n = atoi(bus.arg);
-if (bus.lifi == 0)
-addnode(head, n);
-else
-addqueue(head, n);
+	stack_t *temp_stack;
+
+	temp_stack = *stack;
+	(void)operand;
+
+	while (temp_stack)
+	{
+		printf("%d\n", temp_stack->n);
+		temp_stack = temp_stack->next;
+	}
 }
