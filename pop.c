1d1617710afedfa7832c5a3cb49d1520ebcc257b
gooddids22 <splendidotamiri@gmail.com>
2023-06-21 13:51:21 +0100

    pop.c

--- pop.c
+++ pop.c
@@ -1,23 +1,24 @@
 #include "monty.h"
 /**
- * f_pop - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_pop(stack_t **head, unsigned int counter)
+ * pop_function - Removes the top element of the stack.
+ * @h: Pointer to the head of the stack.
+ * @c: Current line number of the bytecode file.
+ *
+ * Return: void
+ */
+void pop_function(stack_t **h, unsigned int c)
 {
-	stack_t *h;
+	stack_t *temp;
 
-	if (*head == NULL)
+	if (*h == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", c);
+		fclose(data.file_descriptor);
+		free(data.buff);
+		free_td(*h);
+		exit(1);
 	}
-	h = *head;
-	*head = h->next;
-	free(h);
+	temp = *h;
+	*h = temp->next;
+	free(temp);
 }
